f80bd5c411844dfb872d0109b50e7a24181592b7
Jaemin James Kim <jkim557@illinois.edu>
2025-06-12 14:05:17 +0000

    가이드 수정

--- app.py
+++ app.py
@@ -12,30 +12,41 @@ from PIL import Image
 st.write("2030 강서 주말 영어회화")
 st.write("하이강서 @마곡나루")
 st.write("> 그룹 편성 Randomizer")
-
 st.divider()
-st.write(f" 소모임 어플에서 참여 멤버 리스트를 캡쳐 후,") 
-st.write(f" 이미지들을 업로드 해주세요.")
-st.write(f" 1부, 2부 종료 시, 반드시 각각 Update Attendance를 실행해 주세요.")
-st.write(f" 1부 종료 --> Update Attendance")
-st.write(f" 2부 종료 --> Update Attendance")
-st.write(f" ")
-st.write(f" * 1부만 참여하는 인원: ")
-st.write(f" 1) 1부 끝나고 Update Attendance에서 출석 업데이트 후,")
-st.write(f" 2) 캡처 이미지에서 스티커나 그리기 도구로 [1부 only 멤버 이름들] 가리기")
-st.write(f" 3) 마스킹 처리된 캡처 이미지로 Randomizer 실행")
-st.write(f" ")
-st.write(f" * 2부만 참여하는 인원: ")
-st.write(f" 1) 캡처 이미지에서 스티커나 그리기 도구로 이름 가리고 나서 Randomizer 실행")
-st.write(f" 2) 1부 끝나고 Update Attendance에서 출석 업데이트 후,")
-st.write(f" 3) 마스킹 안된 원본 이미지로 Randomizer 실행")
+
+guide_text = """
+* 소모임 어플에서 참여 멤버 리스트를 캡쳐 후, 아래에서 이미지들을 업로드 해주세요. \n \n
+* 1부, 2부 종료 시, 반드시 각각 Update Attendance를 실행해 주세요. \n
+ex) \n
+1부 종료 --> Update Attendance \n
+2부 종료 --> Update Attendance \n
+"""
+def stream_data():
+    for word in guide_text.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+if st.button(f" :sunglasses: **[ Randomizer 사용 가이드 보기 ]** :sunglasses:"):
+    st.write_stream(stream_data)
+    st.write(f" ")
+    with st.expander("* 1부만 참여하는 인원 있을경우 "):
+        st.write(f" 1) 1부 끝나고 Update Attendance에서 출석 업데이트 후,")
+        st.write(f" 2) 캡처 이미지에서 스티커나 그리기 도구로 [1부 only 멤버 이름들] 가리기")
+        st.write(f" 3) 마스킹 처리된 캡처 이미지로 Randomizer 실행")
+        st.write(f" ")
+    with st.expander("* 2부만 참여하는 인원 있을경우 "):
+        st.write(f" 1) 캡처 이미지에서 스티커나 그리기 도구로 이름 가리고 나서 Randomizer 실행")
+        st.write(f" 2) 1부 끝나고 Update Attendance에서 출석 업데이트 후,")
+        st.write(f" 3) 마스킹 안된 원본 이미지로 Randomizer 실행")
+        st.write(f" ")
+
 
 
 tab1, tab2 = st.tabs(["Group Randomizer", "Update Attendance"])
 
 # 조 랜덤 편성
 with tab1:
-    date = st.date_input("Pick a date : 1부는 토요일 날짜, 2부는 일요일 날짜로 입력해주세요", key='randomizer_date_input')
+    date = st.date_input("Pick a date : **1부는 토요일 날짜, 2부는 일요일 날짜로 입력해주세요**", key='randomizer_date_input')
     uploaded_files = st.file_uploader(
         "Choose image files", accept_multiple_files=True
     )
