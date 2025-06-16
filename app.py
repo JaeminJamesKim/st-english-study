9340c4623331e729d784c9075561e176e1c0f761
Jaemin James Kim <jkim557@illinois.edu>
2025-06-16 16:29:38 +0000

    fix4

--- app.py
+++ app.py
@@ -10,18 +10,19 @@ from main import main
 from common.utils import save_updated_attendance, compare_and_update_member_list_excel
 from PIL import Image
 
-st.write("2030 강서 주말 영어회화")
-st.write(f"하이강서 @마곡나루")
-st.write("> 그룹 편성 Randomizer *by James*")
 st.markdown("""
 <style>
 .small-font {
-    font-size:10px !important;
+    font-size:15px !important;
 }
 </style>
 """, unsafe_allow_html=True)
 today_date = datetime.today().strftime('%Y-%m-%d')
 st.markdown(f'> <p class="small-font">Last Updated: {today_date}</p>', unsafe_allow_html=True)
+st.write("2030 강서 주말 영어회화")
+st.write(f"하이강서 @마곡나루")
+st.write("> 그룹 편성 Randomizer *by James*")
+
 
 guide_text = """
 * 소모임 어플에서 참여 멤버 리스트를 캡쳐 후, 아래에서 이미지들을 업로드 해주세요. \n \n
