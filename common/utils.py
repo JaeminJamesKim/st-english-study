6f9d2d6a50336597969336df86f0438cf6ce541f
Jaemin James Kim <jaeminkim1226@hotmail.com>
2025-08-29 20:37:55 +0000

    edited

--- common/utils.py
+++ common/utils.py
@@ -92,7 +92,7 @@ def compare_and_update_member_list_excel(curr_member_name_lst, gc):
         resigned_members = ['없음']
     for member_name in curr_member_name_lst:
         if member_name not in member_list['name']:
-            member_list.loc[len(member_list)] = [member_name, 'M', 0, '하', False]
+            member_list.iloc[[len(member_list)]] = [member_name, 'M', 0, '하', False]
     member_list = member_list[member_list["name"].isin(curr_member_name_lst)].reset_index(drop=True)
     member_list.to_excel('member_list.xlsx') #TODO Check for Release
     
